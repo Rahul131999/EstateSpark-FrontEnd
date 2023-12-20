2f455087faa69becff8c57b8f7993b1be5073db5
Rahul Saharan <rahulsaharan13@gmail.com>
2023-12-20 15:14:14 +0530

    header component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import Profile from './Pages/Profile'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Profile from "./Pages/Profile";
+import Header from "./Components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/profile' element={<Profile/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,38 @@
+import { FaSearch } from "react-icons/fa";
+import {Link} from "react-router-dom"
+
+function Header() {
+  return (
+    <header className="bg-slate-400 shadow-lg">
+      <div className="flex justify-between items-center max-w-6xl m-auto p-3">
+        <Link to='/'>
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-black">Estate</span>
+          <span className="text-orange-500">Spark</span>
+        </h1>
+        </Link>
+        <form className="bg-slate-200 rounded-lg p-3 flex items-center">
+          <input
+            type="search"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          ></input>
+          <FaSearch className="text-slate-700" />
+        </form>
+        <ul className="flex gap-4">
+            <Link to='/'>
+            <li className="hidden sm:inline hover:underline text-slate-700">Home</li>
+            </Link>
+            <Link to='/about'>
+            <li className="hidden sm:inline hover:underline text-slate-700">About</li>
+            </Link>
+            <Link to='/login'> 
+            <li className="hover:underline text-slate-700">Login</li>
+            </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
